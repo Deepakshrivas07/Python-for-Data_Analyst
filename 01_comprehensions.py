8608d639caf922509ab5c2156467b9c96f957faa
deepakshrivas440 <deepakshrivas440@gmail.com>
2026-07-28 00:22:34 +0530

    comprehensions

--- 01_comprehensions.py
+++ 01_comprehensions.py
@@ -18,3 +18,31 @@ for i in nums:
 
 even_no = [i for i in nums if i%2==0]
 print(even_no)
+
+#3rd making dictionary by combining to list
+
+fruits = ["apple","banana","graps"]
+prices = [100,60,40]
+
+fruit_price = {key:value for key,value in zip(fruits,prices)}
+#or
+fruit_price = {fruits[i]:prices[i] for i in range(len(fruits))}
+print(fruit_price)
+
+#3rd filtering dictionary
+
+scores = {"maths":40,"science":50,"hindi":70,"sst":66}
+
+result = {key:value for key,value in scores.items() if value >= 50}
+print(result)
+
+# 4th nesting in comprehension
+
+pairs = [[i,j] for i in [1,2] for j in [3,4] ] # nested list
+print(pairs)
+# above is same as
+for i in [1,2]:
+    for  j in [3,4]:
+        pairs.append(i,j)
+
+print(pairs)

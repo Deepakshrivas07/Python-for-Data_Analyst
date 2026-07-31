9e03e5bb840a22b44c564d38fb35341fe5e26590
deepakshrivas440 <deepakshrivas440@gmail.com>
2026-07-31 19:55:01 +0530

    scatterplot

--- 11_seaborn/01_scatterplot.py
+++ 11_seaborn/01_scatterplot.py
@@ -2,7 +2,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 
-sns.set_style("darkgrid")
+sns.set_style("darkgrid") #to set background(grid)
 tips = sns.load_dataset("tips") #Popular ones:tip,siris,penguins,titanic,diamonds,flights ,mpg
 # print(tips.head()) 
 #hue parameter determines which column in the dataset is used for color encoding to distinguish different categories or groups within the visualization.

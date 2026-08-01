7f8b842d83248abdc3fb36753401a1cb5d562547
deepakshrivas440 <deepakshrivas440@gmail.com>
2026-08-01 20:16:41 +0530

    project-Gurgaon real Estate market analysis

--- 12_Project_Gurgaon_real_state_market_analysis/main.py
+++ 12_Project_Gurgaon_real_state_market_analysis/main.py
@@ -31,6 +31,58 @@ df.drop_duplicates()
 
 #QUESTIONS
 #Question 1: Which is the costliest flat?
-costiest_flat  = df['price'].idxmax()
+costiest_flat  = df['price'].idxmax() #idxmax gives index of max value and as we know loc takes row as index and column as value
+df.loc[costiest_flat]
 # print(costiest_flat) it will print the index of costliest flat but our que is Which is the costliest flat?
-print(df.loc[costiest_flat])
+
+# or we can write in single line 
+costiest_flat = df.loc[df['price'].idxmax()] 
+
+# Question 2: Which locality has the highest average price?
+
+highest_avg_price=  df.groupby('locality')['price'].mean().sort_values(ascending=False).head(1)
+# print(highest_avg_price)
+
+#Question 3: Which locality has the highest rate per square foot?
+
+highest_rate_per_sqfoot = df.groupby('locality')['rate_per_sqft'].mean().sort_values(ascending=False).head(1)
+# print(highest_rate_per_sqfoot)
+
+#Question 4: Ready-to-move vs Under-construction pricing
+ready_vs_under_construction =df.groupby('status')['price'].median() #(optimal as u can see below 3 line code)
+# print(ready_vs_under_construction)
+#or (non optimal)
+under_contruction_price = df[df['status']=='under construction']['price'].median()
+ready_to_move_price = df[df['status']=='ready to move']['price'].median()
+# print(under_contruction_price)
+# print(ready_to_move_price)
+
+#Question 5: Does RERA approval affect pricing?
+rera_approval=df.groupby("rera_approval")["price"].median()
+#or
+rera_approval_true = df[df['rera_approval']=='True']['price'].median()
+rera_approval_false = df[df['rera_approval']=='False']['price'].median()
+# print(rera_approval)
+
+#Question 6: How does area impact price?
+# let see by plotting
+# sns.scatterplot(x='area',y='price',data=df)
+# plt.show() #ans is no location affects the price not area
+
+#Question 7: Which BHK configuration is most expensive? 
+most_expensive_bhk = df.groupby("bhk_count")["price"].mean().idxmax() 
+# print(f"most_expensive_bhk is {most_expensive_bhk} BHK configuration")
+
+#Question 8 Which property type is the costliest?
+most_expensive_property_type = df.groupby('property_type')['price'].max().sort_values(ascending=False).head(1)
+print(most_expensive_property_type)
+
+#Question 9: Do certain builders price higher?
+builders_price = df.groupby('company_name')['price'].mean().sort_values(ascending=False)
+# print(builders_price)
+
+
+#Question 10: Are larger homes more expensive per sqft?
+
+sns.scatterplot(data = df , x = 'area', y = 'rate_per_sqft')
+plt.show()
